55fac29b07d2bf948ccfbcbbe26513a54824f516
Junard Oclarit <junard.o@dvmelite.com>
2020-11-06 19:50:04 +0800

    Move upbook to static/third

---
+++ static/third/upbook/dist/common.3ffa157e0085ae6791ca.js
@@ -0,0 +1,321 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["common"],{
+
+/***/ "./node_modules/ngx-bootstrap/buttons/button-checkbox.directive.js":
+/*!*************************************************************************!*\
+  !*** ./node_modules/ngx-bootstrap/buttons/button-checkbox.directive.js ***!
+  \*************************************************************************/
+/*! exports provided: CHECKBOX_CONTROL_VALUE_ACCESSOR, ButtonCheckboxDirective */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CHECKBOX_CONTROL_VALUE_ACCESSOR", function() { return CHECKBOX_CONTROL_VALUE_ACCESSOR; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ButtonCheckboxDirective", function() { return ButtonCheckboxDirective; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
+
+
+// TODO: config: activeClass - Class to apply to the checked buttons
+var CHECKBOX_CONTROL_VALUE_ACCESSOR = {
+    provide: _angular_forms__WEBPACK_IMPORTED_MODULE_1__["NG_VALUE_ACCESSOR"],
+    useExisting: Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["forwardRef"])(function () { return ButtonCheckboxDirective; }),
+    multi: true
+};
+/**
+ * Add checkbox functionality to any element
+ */
+var ButtonCheckboxDirective = /** @class */ /*@__PURE__*/ (function () {
+    function ButtonCheckboxDirective() {
+        /** Truthy value, will be set to ngModel */
+        this.btnCheckboxTrue = true;
+        /** Falsy value, will be set to ngModel */
+        this.btnCheckboxFalse = false;
+        this.state = false;
+        this.onChange = Function.prototype;
+        this.onTouched = Function.prototype;
+    }
+    // view -> model
+    ButtonCheckboxDirective.prototype.onClick =
+        // view -> model
+        function () {
+            if (this.isDisabled) {
+                return;
+            }
+            this.toggle(!this.state);
+            this.onChange(this.value);
+        };
+    ButtonCheckboxDirective.prototype.ngOnInit = function () {
+        this.toggle(this.trueValue === this.value);
+    };
+    Object.defineProperty(ButtonCheckboxDirective.prototype, "trueValue", {
+        get: function () {
+            return typeof this.btnCheckboxTrue !== 'undefined'
+                ? this.btnCheckboxTrue
+                : true;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(ButtonCheckboxDirective.prototype, "falseValue", {
+        get: function () {
+            return typeof this.btnCheckboxFalse !== 'undefined'
+                ? this.btnCheckboxFalse
+                : false;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    ButtonCheckboxDirective.prototype.toggle = function (state) {
+        this.state = state;
+        this.value = this.state ? this.trueValue : this.falseValue;
+    };
+    // ControlValueAccessor
+    // model -> view
+    // ControlValueAccessor
+    // model -> view
+    ButtonCheckboxDirective.prototype.writeValue =
+        // ControlValueAccessor
+        // model -> view
+        function (value) {
+            this.state = this.trueValue === value;
+            this.value = value ? this.trueValue : this.falseValue;
+        };
+    ButtonCheckboxDirective.prototype.setDisabledState = function (isDisabled) {
+        this.isDisabled = isDisabled;
+    };
+    ButtonCheckboxDirective.prototype.registerOnChange = function (fn) {
+        this.onChange = fn;
+    };
+    ButtonCheckboxDirective.prototype.registerOnTouched = function (fn) {
+        this.onTouched = fn;
+    };
+    return ButtonCheckboxDirective;
+}());
+
+
+
+
+
+
+/***/ }),
+
+/***/ "./node_modules/ngx-bootstrap/buttons/button-radio-group.directive.js":
+/*!****************************************************************************!*\
+  !*** ./node_modules/ngx-bootstrap/buttons/button-radio-group.directive.js ***!
+  \****************************************************************************/
+/*! exports provided: RADIO_CONTROL_VALUE_ACCESSOR, ButtonRadioGroupDirective */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "RADIO_CONTROL_VALUE_ACCESSOR", function() { return RADIO_CONTROL_VALUE_ACCESSOR; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ButtonRadioGroupDirective", function() { return ButtonRadioGroupDirective; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
+
+
+var RADIO_CONTROL_VALUE_ACCESSOR = {
+    provide: _angular_forms__WEBPACK_IMPORTED_MODULE_1__["NG_VALUE_ACCESSOR"],
+    useExisting: Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["forwardRef"])(function () { return ButtonRadioGroupDirective; }),
+    multi: true
+};
+/**
+ * A group of radio buttons.
+ * A value of a selected button is bound to a variable specified via ngModel.
+ */
+var ButtonRadioGroupDirective = /** @class */ /*@__PURE__*/ (function () {
+    function ButtonRadioGroupDirective(el, cdr) {
+        this.el = el;
+        this.cdr = cdr;
+        this.onChange = Function.prototype;
+        this.onTouched = Function.prototype;
+    }
+    Object.defineProperty(ButtonRadioGroupDirective.prototype, "value", {
+        get: function () {
+            return this._value;
+        },
+        set: function (value) {
+            this._value = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    ButtonRadioGroupDirective.prototype.writeValue = function (value) {
+        this._value = value;
+        this.cdr.markForCheck();
+    };
+    ButtonRadioGroupDirective.prototype.registerOnChange = function (fn) {
+        this.onChange = fn;
+    };
+    ButtonRadioGroupDirective.prototype.registerOnTouched = function (fn) {
+        this.onTouched = fn;
+    };
+    return ButtonRadioGroupDirective;
+}());
+
+
+
+
+
+
+/***/ }),
+
+/***/ "./node_modules/ngx-bootstrap/buttons/button-radio.directive.js":
+/*!**********************************************************************!*\
+  !*** ./node_modules/ngx-bootstrap/buttons/button-radio.directive.js ***!
+  \**********************************************************************/
+/*! exports provided: RADIO_CONTROL_VALUE_ACCESSOR, ButtonRadioDirective */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "RADIO_CONTROL_VALUE_ACCESSOR", function() { return RADIO_CONTROL_VALUE_ACCESSOR; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ButtonRadioDirective", function() { return ButtonRadioDirective; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
+/* harmony import */ var _button_radio_group_directive__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./button-radio-group.directive */ "./node_modules/ngx-bootstrap/buttons/button-radio-group.directive.js");
+
+
+
+var RADIO_CONTROL_VALUE_ACCESSOR = {
+    provide: _angular_forms__WEBPACK_IMPORTED_MODULE_1__["NG_VALUE_ACCESSOR"],
+    useExisting: Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["forwardRef"])(function () { return ButtonRadioDirective; }),
+    multi: true
+};
+/**
+ * Create radio buttons or groups of buttons.
+ * A value of a selected button is bound to a variable specified via ngModel.
+ */
+var ButtonRadioDirective = /** @class */ /*@__PURE__*/ (function () {
+    function ButtonRadioDirective(el, cdr, group, renderer) {
+        this.el = el;
+        this.cdr = cdr;
+        this.group = group;
+        this.renderer = renderer;
+        this.onChange = Function.prototype;
+        this.onTouched = Function.prototype;
+    }
+    Object.defineProperty(ButtonRadioDirective.prototype, "value", {
+        get: /** Current value of radio component or group */ function () {
+            return this.group ? this.group.value : this._value;
+        },
+        set: function (value) {
+            if (this.group) {
+                this.group.value = value;
+                return;
+            }
+            this._value = value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(ButtonRadioDirective.prototype, "disabled", {
+        get: /** If `true` — radio button is disabled */ function () {
+            return this._disabled;
+        },
+        set: function (disabled) {
+            this._disabled = disabled;
+            this.setDisabledState(disabled);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(ButtonRadioDirective.prototype, "isActive", {
+        get: function () {
+            return this.btnRadio === this.value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    ButtonRadioDirective.prototype.onClick = function () {
+        if (this.el.nativeElement.attributes.disabled || !this.uncheckable && this.btnRadio === this.value) {
+            return;
+        }
+        this.value = this.uncheckable && this.btnRadio === this.value ? undefined : this.btnRadio;
+        this._onChange(this.value);
+    };
+    ButtonRadioDirective.prototype.ngOnInit = function () {
+        this.uncheckable = typeof this.uncheckable !== 'undefined';
+    };
+    ButtonRadioDirective.prototype.onBlur = function () {
+        this.onTouched();
+    };
+    ButtonRadioDirective.prototype._onChange = function (value) {
+        if (this.group) {
+            this.group.onTouched();
+            this.group.onChange(value);
+            return;
+        }
+        this.onTouched();
+        this.onChange(value);
+    };
+    // ControlValueAccessor
+    // model -> view
+    // ControlValueAccessor
+    // model -> view
+    ButtonRadioDirective.prototype.writeValue =
+        // ControlValueAccessor
+        // model -> view
+        function (value) {
+            this.value = value;
+            this.cdr.markForCheck();
+        };
+    ButtonRadioDirective.prototype.registerOnChange = function (fn) {
+        this.onChange = fn;
+    };
+    ButtonRadioDirective.prototype.registerOnTouched = function (fn) {
+        this.onTouched = fn;
+    };
+    ButtonRadioDirective.prototype.setDisabledState = function (disabled) {
+        if (disabled) {
+            this.renderer.setAttribute(this.el.nativeElement, 'disabled', 'disabled');
+            return;
+        }
+        this.renderer.removeAttribute(this.el.nativeElement, 'disabled');
+    };
+    return ButtonRadioDirective;
+}());
+
+
+
+
+
+
+/***/ }),
+
+/***/ "./node_modules/ngx-bootstrap/buttons/buttons.module.js":
+/*!**************************************************************!*\
+  !*** ./node_modules/ngx-bootstrap/buttons/buttons.module.js ***!
+  \**************************************************************/
+/*! exports provided: ButtonsModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ButtonsModule", function() { return ButtonsModule; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _button_checkbox_directive__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./button-checkbox.directive */ "./node_modules/ngx-bootstrap/buttons/button-checkbox.directive.js");
+/* harmony import */ var _button_radio_directive__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./button-radio.directive */ "./node_modules/ngx-bootstrap/buttons/button-radio.directive.js");
+/* harmony import */ var _button_radio_group_directive__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./button-radio-group.directive */ "./node_modules/ngx-bootstrap/buttons/button-radio-group.directive.js");
+
+
+
+
+var ButtonsModule = /** @class */ /*@__PURE__*/ (function () {
+    function ButtonsModule() {
+    }
+    ButtonsModule.forRoot = function () {
+        return { ngModule: ButtonsModule, providers: [] };
+    };
+    return ButtonsModule;
+}());
+
+
+
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=common.3ffa157e0085ae6791ca.js.map
