4a345c321e5564366ba3cb12dfb0f3d1f461fe44
Junard Oclarit <junard.o@dvmelite.com>
2020-11-10 02:59:34 +0800

    update webpack.

--- tools/webpack.config.ts
+++ tools/webpack.config.ts
@@ -242,16 +242,16 @@ export default (env?: string): webpack.Configuration[] => {
     // Good error messages show up in production and development in the source maps
     const exposeOptions = [
         // {path: "./debug-require.js", name: "__require"},
-        {path: "blueimp-md5/js/md5.js"},
-        {path: "clipboard/dist/clipboard.js", name: "ClipboardJS"},
-        {path: "xdate/src/xdate.js", name: "XDate"},
-        {path: "../static/third/marked/lib/marked.js"},
-        {path: "../static/js/debug.js"},
+        // {path: "blueimp-md5/js/md5.js"},
+        // {path: "clipboard/dist/clipboard.js", name: "ClipboardJS"},
+        // {path: "xdate/src/xdate.js", name: "XDate"},
+        // {path: "../static/third/marked/lib/marked.js"},
+        // {path: "../static/js/debug.js"},
         {path: "jquery/dist/jquery.js", name: ["$", "jQuery"]},
         // {path: "underscore/underscore.js", name: "_"},
-        {path: "handlebars/dist/cjs/handlebars.runtime.js", name: "Handlebars"},
-        {path: "sortablejs/Sortable.js"},
-        {path: "winchan/winchan.js", name: "WinChan"},
+        // {path: "handlebars/dist/cjs/handlebars.runtime.js", name: "Handlebars"},
+        // {path: "sortablejs/Sortable.js"},
+        // {path: "winchan/winchan.js", name: "WinChan"},
     ];
     config.module.rules.unshift(...getExposeLoaders(exposeOptions));
 
